ead1467ebfaf2254325be49558221c17f3ae54e4
JK7k <97955908+JK7k@users.noreply.github.com>
2023-02-08 14:53:09 +0100

    Add files via upload

---
+++ 4tie.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 08 Lut 2023, 14:52
+-- Wersja serwera: 10.4.19-MariaDB
+-- Wersja PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `4tie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dane`
+--
+
+CREATE TABLE `dane` (
+  `id` int(11) NOT NULL,
+  `imie` text NOT NULL,
+  `nazwisko` text NOT NULL,
+  `pesel` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `dane`
+--
+
+INSERT INTO `dane` (`id`, `imie`, `nazwisko`, `pesel`) VALUES
+(1, 'as', '', ''),
+(2, 'as', '', ''),
+(3, 'fdsf', '231312312', ''),
+(4, 'Kacper', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `oceny`
+--
+
+CREATE TABLE `oceny` (
+  `id` int(11) NOT NULL,
+  `idosoby` int(11) NOT NULL,
+  `idprzedmiotu` int(11) NOT NULL,
+  `ocena` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `oceny`
+--
+
+INSERT INTO `oceny` (`id`, `idosoby`, `idprzedmiotu`, `ocena`) VALUES
+(1, 3, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `przedmiot`
+--
+
+CREATE TABLE `przedmiot` (
+  `ID` int(11) NOT NULL,
+  `przedmiot` varchar(45) NOT NULL,
+  `nauczyciel` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `przedmiot`
+--
+
+INSERT INTO `przedmiot` (`ID`, `przedmiot`, `nauczyciel`) VALUES
+(1, 'matematyka', 'Agata Zabadaj'),
+(2, 'język angielski', 'Bożena Czakon'),
+(3, 'informatyka', 'Barbara Woszczycka'),
+(4, 'język polski', 'Monika Kowalska'),
+(5, 'chemia', 'Magdalena Woźnica');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `dane`
+--
+ALTER TABLE `dane`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `oceny`
+--
+ALTER TABLE `oceny`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `przedmiot`
+--
+ALTER TABLE `przedmiot`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `dane`
+--
+ALTER TABLE `dane`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `oceny`
+--
+ALTER TABLE `oceny`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `przedmiot`
+--
+ALTER TABLE `przedmiot`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
